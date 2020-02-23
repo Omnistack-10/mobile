647f582b790648bb0732d4297204ca4fc287d018
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 17:55:42 -0300

    ajustando rotas e statusBar

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Routes from './src/Routes';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
 }
