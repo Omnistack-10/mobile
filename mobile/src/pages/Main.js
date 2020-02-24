ab2fd5e24220de45473a306ee0e085d5f77b2340
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 21:29:02 -0300

    adicionado estilos ao input e botao de localizacao

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-import { MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
-    useEffect(()=> {
+    useEffect(() => {
         async function loadInitialPosition() {
             const { granted } = await requestPermissionsAsync();
 
@@ -37,23 +37,23 @@ function Main({ navigation }) {
     return (
         <>
             <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -19.5460251, longitude: -42.6586987 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/45008136?s=460&v=4' }} />
+                <Marker coordinate={{ latitude: -19.5460251, longitude: -42.6586987 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/45008136?s=460&v=4' }} />
 
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'mayconcarvalho' }) ;
-                }} >
-                    <View style={styles.callout}>
-                        <Text style={styles.devName} >Maycon Arthuso</Text>
-                        <Text style={styles.devBio} >bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio </Text>
-                        <Text style={styles.devTechs} >ReactJs, React Native, Nodejs</Text>
-                    </View>
-                </Callout>
-            </Marker> 
-        </MapView>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'mayconcarvalho' });
+                    }} >
+                        <View style={styles.callout}>
+                            <Text style={styles.devName} >Maycon Arthuso</Text>
+                            <Text style={styles.devBio} >bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio bio </Text>
+                            <Text style={styles.devTechs} >ReactJs, React Native, Nodejs</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
 
             <View style={styles.searchForm}>
-                <TextInput 
+                <TextInput
                     style={styles.searchInput}
                     placeholder="Buscar devs por techs..."
                     placeholderTextColor="#999"
@@ -61,12 +61,12 @@ function Main({ navigation }) {
                     autoCorrect={false}
                 />
 
-                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
                     <MaterialIcons name="my-location" size={20} color="#FFF" />
                 </TouchableOpacity>
             </View>
         </>
-    );    
+    );
 }
 
 const styles = StyleSheet.create({
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 4,
         borderColor: '#FFF'
-    }, 
+    },
 
     callout: {
         width: 260
@@ -98,7 +98,44 @@ const styles = StyleSheet.create({
 
     devTechs: {
         marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
     }
+
 });
 
 export default Main;
