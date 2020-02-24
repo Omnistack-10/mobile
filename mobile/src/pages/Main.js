f42ff9598b0eb4a52c4c57a1941af87d78ced0b7
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 22:06:17 -0300

    ajustando a busca pelo input de techs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -9,6 +9,7 @@ import api from '../services/api';
 function Main({ navigation }) {
     const [devs, setDevs] = useState([]);
     const [currentRegion, setCurrentRegion] = useState(null);
+    const [techs, setTechs] = useState('');
 
     useEffect(() => {
         async function loadInitialPosition() {
@@ -40,7 +41,7 @@ function Main({ navigation }) {
             params: {
                 latitude,
                 longitude,
-                techs: 'ReactJs'
+                techs
             }
         });
 
@@ -94,6 +95,8 @@ function Main({ navigation }) {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
 
                 <TouchableOpacity onPress={ loadDevs } style={styles.loadButton}>
