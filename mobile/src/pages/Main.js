248b8534956252de7853b554a24c6dcb4ae72e10
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2020-02-23 21:18:48 -0300

    Adicionando input text de techs no mapa

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -34,7 +35,8 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
             <Marker coordinate={{ latitude: -19.5460251, longitude: -42.6586987 }}>
                 <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/45008136?s=460&v=4' }} />
 
@@ -49,6 +51,21 @@ function Main({ navigation }) {
                 </Callout>
             </Marker> 
         </MapView>
+
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#FFF" />
+                </TouchableOpacity>
+            </View>
+        </>
     );    
 }
 
